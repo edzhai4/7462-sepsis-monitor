801e8793d5d7aed1de7c952f07485ce0d00b9a45
Edward Zhai <zhaix069@umn.edu>
2023-04-09 10:00:38 -0500

    Cleaning up failed attempts

--- my_script.R
+++
@@ -1,5 +0,0 @@
-library(googledrive)
-library(googleAuthR)
-
-# Set the authentication configuration using the service account method
-googleAuthR::gar_service(json = Sys.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
